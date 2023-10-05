14a437652152a931f20588ca7261e868ec1f8049
Jishan Ahamed <jjahamed33@gmail.com>
2023-10-06 01:26:41 +0530

    Add project files.

---
+++ JX Travel Agency Web App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JX_Travel_Agency_Web_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
